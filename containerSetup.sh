c9f02896323beb4f29df5fd4dc511e66d8aba7b8
orik <oriknj999@gmail.com>
2022-05-22 16:57:09 +0700

    removed fail2ban bug fixes

--- containerSetup.sh
+++ containerSetup.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 echo -e '\033[5m\nWARNING WARNING WARNING\033[0m'
-echo '\nWARNING: run this script only once a user has been created as this will disable root login'
+printf '\nWARNING: run this script only once a user has been created as this will disable root login'
 
 while true; do
-read -p "Do you want to proceed? (y/n) " yn
+read -p "Do you want to proceed? (Y/n) " yn
 case $yn in 
-	[yY] ) echo ok, we will proceed;
+	[""|yY] ) echo ok, we will proceed;
 		break;;
 	[nN] ) echo exiting...;
 		exit;;
@@ -18,15 +18,15 @@ done
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g;s/#AddressFamily any/AddressFamily inet/g' /etc/ssh/sshd_config
     #   Make ssh folder and set permissions
 mkdir ~/.ssh && chmod 700 ~/.ssh
-    #   Install Start Fail2Ban
-apt install fail2ban && systemctl start fail2ban
-    #   Create Copy fail2ban jail.local config
-cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-    #   Whitelist yourself from ever getting jailed
-sed -i 's!#ignoreip = 127.0.0.1/8 ::1!ignoreip = 127.0.0.1/8 ::1 192.168.1.100 192.168.1.101!g' /etc/fail2ban/jail.local
-    #   Clean downloaded packages and remove orphans
+#    #   Install Start Fail2Ban
+#apt install fail2ban && systemctl start fail2ban
+#    #   Create Copy fail2ban jail.local config
+#cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+#    #   Whitelist yourself from ever getting jailed
+#sed -i 's!#ignoreip = 127.0.0.1/8 ::1!ignoreip = 127.0.0.1/8 ::1 192.168.1.100 192.168.1.101!g' /etc/#fail2ban/jail.local
+#    #   Clean downloaded packages and remove orphans
 apt clean && sudo apt autoremove
     #   Set system for clean first boot setup
 rm /etc/ssh/ssh_host_*
 truncate -s 0 /etc/machine-id
-echo 'Finished, shutdown and create template'
+printf '\nFinished, shutdown and create template'
