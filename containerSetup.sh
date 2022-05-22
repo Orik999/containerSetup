0ede98fd1a40693f31216f40abb1929385254a73
orik <oriknj999@gmail.com>
2022-05-22 15:27:19 +0700

    fixed sed dir for jail2ban

--- containerSetup.sh
+++ containerSetup.sh
@@ -23,7 +23,7 @@ apt install fail2ban && systemctl start fail2ban
     #   Create Copy fail2ban jail.local config
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
     #   Whitelist yourself from ever getting jailed
-sed -i 's/#ignoreip = 127.0.0.1/8 ::1/ignoreip = 127.0.0.1/8 ::1 192.168.1.100 192.168.1.101/g' /jail.local
+sed -i 's!#ignoreip = 127.0.0.1/8 ::1!ignoreip = 127.0.0.1/8 ::1 192.168.1.100 192.168.1.101!g' /etc/fail2ban/jail.local
     #   Clean downloaded packages and remove orphans
 apt clean && sudo apt autoremove
     #   Set system for clean first boot setup
