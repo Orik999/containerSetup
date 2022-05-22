1eab9433a4457a385fb0465e59d04f3afa9d15ab
orik <oriknj999@gmail.com>
2022-05-22 15:00:02 +0700

    fixed bash yes/no errors

--- containerSetup.sh
+++ containerSetup.sh
@@ -23,13 +23,9 @@ apt install fail2ban && systemctl start fail2ban
     #   Create Copy fail2ban jail.local config
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
     #   Whitelist yourself from ever getting jailed
-sed -i 's/#ignoreip = 127.0.0.1/8 ::1/ignoreip = 127.0.0.1/8 ::1 192.168.1.100 192.168.1.101/g' /etc/sddm.conf
+sed -i 's/#ignoreip = 127.0.0.1/8 ::1/ignoreip = 127.0.0.1/8 ::1 192.168.1.100 192.168.1.101/g' jail.local
     #   Clean downloaded packages and remove orphans
 apt clean && sudo apt autoremove
     #   Set system for clean first boot setup
 cloud-init clean
 ehco 'Finished, Shutdown and create template'
-
-
-
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g;s/#AddressFamily any/AddressFamily inet/g' /etc/ssh/sshd_config
