c9ad8b689fdbb3c73a96399b742d8735cf9b7cbb
orik <oriknj999@gmail.com>
2022-05-23 11:39:29 +0700

    more bug fixes v2.1

--- containerSetup.sh
+++ containerSetup.sh
@@ -7,7 +7,7 @@ read -p "[1] Setup Container(Root) - [2] Setup Container Template(User) - [c] Ca
     case $response12c in 
         [1] ) echo Setting up Container;
         #   Check if user exists
-            egrep "$username" /etc/passwd >/dev/null || echo "Error- Container is not a template" && exit 1
+            egrep "$username" /etc/passwd >/dev/null || echo "Error- Container is not a template" && exit 1;
         #   Delete old ssh keys and gen new keys
             rm /etc/ssh/ssh_host_* && dpkg-reconfigure openssh-server
         #   Disable Password login
@@ -16,33 +16,34 @@ read -p "[1] Setup Container(Root) - [2] Setup Container Template(User) - [c] Ca
             sudo apt update && sudo apt -y dist-upgrade
         #   Clean downloaded packages and remove orphans
             apt clean && sudo apt autoremove
-            read -p "Reboot now. "
+            read -n 1 -s -r -p "Reboot now. "
             rp=1
             break;;
         [2] ) echo Setting up Container Template;
             #read -p "Enter Username" username
             #read -p "Enter Password" password
-            password=sa.EukkiViW5.
         #   Check if user exists > exit
-            egrep "$username" /etc/passwd >/dev/null && echo "User exists! Script cancelled" && exit 1
+            egrep "$username" /etc/passwd >/dev/null && echo "User exists! Script cancelled" && exit 1;
+        #   Set hashed pass
+            password=sa.EukkiViW5.
         #   Create user
             useradd -m -p "$password" "$username"
         #   Add user to sudo group
             usermod -aG sudo $username
-        #   Update packeges
-            sudo apt update && sudo apt -y dist-upgrade
-        #   Restrict Root login and to IPv4 only
-            sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g;s/#AddressFamily any/AddressFamily inet/g' /etc/ssh/sshd_config
         #   Set ssh permissions
             chmod 700 ~/.ssh
         #   Create ssh folder for USER and Set permissions
             mkdir /home/$username/.ssh && chmod 700 /home/$username/.ssh
-            chown /home/$username/.ssh $username
+            chown $username /home/$username/.ssh
+        #   Update packeges
+            sudo apt update && sudo apt -y dist-upgrade
+        #   Restrict Root login and to IPv4 only
+            sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g;s/#AddressFamily any/AddressFamily inet/g' /etc/ssh/sshd_config
         #   Clean downloaded packages and remove orphans
             apt clean && sudo apt autoremove
         #   Set system for clean first boot setup
             truncate -s 0 /etc/machine-id
-            read -p "Power off now. Dont forget to create Template before next boot! "
+            read -n 1 -s -r -p "Power off now. Dont forget to create Template before next boot! "
             rp=2
             break;;
         ""|"c"|"C" ) echo exiting...;
