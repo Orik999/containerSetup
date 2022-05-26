8ecbc9e78b64050a629bfe3befa2f5089840057d
orik <oriknj999@gmail.com>
2022-05-26 15:24:28 +0700

    removed user addition

--- containerVMsetup.sh
+++ containerVMsetup.sh
@@ -40,40 +40,44 @@ EOF
         rp=1
         break;;
     [2] ) echo Setting up VM;
-    #   Check if user exists > exit
-        if  id "$username" >/dev/null 2>&1; then
-            echo "Not a fresh system! Script cancelled"
-            exit 1
-        fi
-    #   If ssh auth key file doesn't exist ask user to enter ssh public key
-        [ -f ~/.ssh/authorized_keys ] || read -p "Must enter ssh public key, otherwise you won't be able to login!: " pubsshkey
-    #   Set hashed pass
-        password=sa.EukkiViW5.
-    #   Create user
-        useradd -m -p "$password" "$username"
-    #   Add user to sudo group
-        usermod -aG sudo $username
-    #   Fix default bash for user
-        chsh -s /bin/bash $username
-    #   Set ssh permissions
-        chmod 700 ~/.ssh
-    #   Create ssh folder for USER and Set permissions
-        mkdir /home/$username/.ssh && chmod 700 /home/$username/.ssh
-        chown $username /home/$username/.ssh
+#    #   Check if user exists > exit
+#        if  id "$username" >/dev/null 2>&1; then
+#            echo "Not a fresh system! Script cancelled"
+#            exit 1
+#        fi
+#    #   If ssh auth key file doesn't exist ask user to enter ssh public key
+#        [ -f ~/.ssh/authorized_keys ] || read -p "Must enter ssh public key, otherwise you won't be able to login!: " pubsshkey
+#    #   Set hashed pass
+#        password=sa.EukkiViW5.
+#    #   Create user
+#        useradd -m -p "$password" "$username"
+#    #   Add user to sudo group
+#        usermod -aG sudo $username
+#    #   Fix default bash for user
+#        chsh -s /bin/bash $username
+#    #   Set ssh permissions
+#        chmod 700 ~/.ssh
+#    #   Create ssh folder for USER and Set permissions
+#        mkdir /home/$username/.ssh && chmod 700 /home/$username/.ssh
+#        chown $username /home/$username/.ssh
     #   Disable root and pass login and to IPv4 only
-        sed -i 's/#AddressFamily any/AddressFamily inet/g;s/#PasswordAuthentication yes/PasswordAuthentication no/g;s/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
+        $SUDO_CMD sed -i 's/#AddressFamily any/AddressFamily inet/g;s/#PasswordAuthentication yes/PasswordAuthentication no/g;s/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
     #   Update packeges
-        apt update && apt -y dist-upgrade
+        $SUDO_CMD apt update && $SUDO_CMD apt -y dist-upgrade
     #   Install qemu agent
-        apt install qemu-guest-agent
+        $SUDO_CMD apt install qemu-guest-agent
     #   Clean downloaded packages and remove orphans
-        apt clean && apt autoremove
-    #   If var pubsshkey not empty create ssh auth file and add ssh public key
-        [[ ! -z "$pubsshkey" ]] && cat > /home/$username/.ssh/authorized_keys << EOF
-# --- BEGIN PVE ---
-$pubsshkey
-# --- END PVE ---
-EOF
+        $SUDO_CMD apt clean && $SUDO_CMD apt autoremove
+#    #   If var pubsshkey not empty create ssh auth file and add ssh public key
+#        [[ ! -z "$pubsshkey" ]] && cat > /home/$username/.ssh/authorized_keys << EOF
+## --- BEGIN PVE ---
+#$pubsshkey
+## --- END PVE ---
+#EOF
+
+# import keys from github
+#ssh-import-id-gh <username>
+
         echo ""
         read -n 1 -s -r -p "Press enter to POWER OFF now. "
         rp=2
