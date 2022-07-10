641c5f53c73bc2ad84c1d9c1afd152e57cf834f1
orik <oriknj999@gmail.com>
2022-07-11 00:47:49 +0700

    Homelab Setup scripts

--- proxmoxSetup.sh
+++ proxmoxSetup.sh
@@ -86,9 +86,9 @@ msg_ok "Added / Corrected PVE7 Sources"
 #   update and clean
 msg_info "Updating system"
 sleep 2
-apt update &>/dev/null
-apt -y dist-upgrade &>/dev/null
-apt -y autoremove &>/dev/null
+apt update # &>/dev/null
+apt -y dist-upgrade # &>/dev/null
+apt -y autoremove # &>/dev/null
 msg_ok "System updated"
 
 msg_info "Disabling Subscription Nag"
